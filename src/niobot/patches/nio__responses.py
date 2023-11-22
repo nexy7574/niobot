f663b667c606ccfbc74dd25606967ba5a5cd00a0
nex <me@nexy7574.co.uk>
2023-11-22 18:34:59 +0000

    Only use DirectRooms patch when the Nio version doesn't have it

--- src/niobot/patches/nio__responses.py
+++ src/niobot/patches/nio__responses.py
@@ -2,32 +2,34 @@
 from dataclasses import dataclass, field
 from typing import Any, Dict, List, Union
 
-from nio.responses import ErrorResponse, Response
+try:
+    from nio.responses import DirectRoomsErrorResponse, DirectRoomsResponse
+except ImportError:
+    from nio.responses import ErrorResponse, Response
+
+    class DirectRoomsErrorResponse(ErrorResponse):
+        pass
+
+    @dataclass
+    class DirectRoomsResponse(Response):
+        """A response containing a list of direct rooms.
+        Attributes:
+            rooms (List[str]): The rooms joined by the account.
+        """
+
+        rooms: Dict[str, List[str]] = field()
+
+        @classmethod
+        def from_dict(
+            cls,
+            parsed_dict: Dict[Any, Any],
+        ) -> Union["DirectRoomsResponse", DirectRoomsErrorResponse]:
+            if parsed_dict.get("errcode") is not None:
+                return DirectRoomsErrorResponse.from_dict(parsed_dict)
+            return cls(parsed_dict)
+
 
 __all__ = (
     "DirectRoomsErrorResponse",
     "DirectRoomsResponse",
 )
-
-
-class DirectRoomsErrorResponse(ErrorResponse):
-    pass
-
-
-@dataclass
-class DirectRoomsResponse(Response):
-    """A response containing a list of direct rooms.
-    Attributes:
-        rooms (List[str]): The rooms joined by the account.
-    """
-
-    rooms: Dict[str, List[str]] = field()
-
-    @classmethod
-    def from_dict(
-        cls,
-        parsed_dict: Dict[Any, Any],
-    ) -> Union["DirectRoomsResponse", DirectRoomsErrorResponse]:
-        if parsed_dict.get("errcode") is not None:
-            return DirectRoomsErrorResponse.from_dict(parsed_dict)
-        return cls(parsed_dict)
